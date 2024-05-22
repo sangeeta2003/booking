6b4f5c831efe94c3ecc01a41b03277dd11f29508
*pknowledge* <mishrasangeeta944@gmail.com>
2024-05-22 19:12:37 +0530

    Header.js init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
-import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
+  import './index.css';
+  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+  import Home from './pages/Home';
+  import Login from './pages/Login';
+  import Layout from './pages/Layout';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+  function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Layout />} />
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 
-export default App;
+  export default App;
---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+import { Outlet } from "react-router-dom"
+const Layout = () => {
+  return (
+    <div>
+        <Header/>
+        <Outlet/>
+    </div>
+  )
+}
+
+export default Layout
