84e8615bc2518a2a0d59d474137b9da9a73294c7
*pknowledge* <mishrasangeeta944@gmail.com>
2024-05-22 19:19:55 +0530

    import the home and login page inside the Layout page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
-  import './index.css';
-  import { BrowserRouter, Routes, Route } from 'react-router-dom';
-  import Home from './pages/Home';
-  import Login from './pages/Login';
-  import Layout from './pages/Layout';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Layout from './pages/Layout';
 
-  function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-        <Route path="/" element={<Layout />} />
-          <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+        <Route path='/' element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        </Route>
+       
 
-  export default App;
+       
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
