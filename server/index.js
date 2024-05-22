a0967deee7d2101c5f3e0724475e2fa5a6a3eea7
*pknowledge* <mishrasangeeta944@gmail.com>
2024-05-22 21:31:58 +0530

    mongoose schema

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const User =require('./models/user')
+const bcrypt = require('bcryptjs')
 const app =  express();
+const bcryptSalt = bcrypt.genSaltSync(10);
 app.use(express.json());
 app.use(cors({
     credentials:true,
 origin:'http://localhost:5173',
 }));
-mongoose
+ mongoose.connect(process.env.MONGO_URL)
 app.get('/test',(req,res)=>{
     res.json('test ok')
 });
-app.post('/register',(req,res)=>{
+app.post('/register',async(req,res)=>{
     const{name,email,password} = req.body;
-    res.json({name,email,password})
+  const userdoc =  await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password,bcryptSalt)
+    })
+    res.json(userdoc)
 })
 
 app.listen(4000);
