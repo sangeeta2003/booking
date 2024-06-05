f9bec00e8d81345399a5a51af638f497017ef734
*pknowledge* <mishrasangeeta944@gmail.com>
2024-06-05 12:39:12 +0530

    placepage fronted

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -18,15 +18,31 @@ const PlacePage = () => {
         {action === 'new' &&(
             
 
-           <form className="flex flex-col ">
+           <form>
             <h2 className="text-2xl mt-4">Title</h2>
             <p className="text-gray-500 text-sm">Title for your place and be confort for this</p>
             <input type="text" placeholder="title,for example: My "/>
-            <h2 className="text-2xl">Address</h2>
+            <h2 className="text-2xl mt-4">Address</h2>
             <p className="text-gray-500 text-sm">Address to this place</p>
             <input type="text" placeholder="address"/>
-            <h2 className="text-2xl">Photos</h2>
-            <p className="text-gray-500 text-sm">Title for your place and be confort for this</p>
+            <h2 className="text-2xl mt-4">Photos</h2>
+            <p className="text-gray-500 text-sm">more == better</p>
+            <div className="flex gap-2">
+                <input type="text" placeholder="Add using a link...jpg"/>
+                <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;Photo</button>
+            </div>
+            <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 mt-4">
+            <button className="flex justify-center gap-1
+            border bg-transparent rounded-2xl p-4 text-2xl text-gray-500">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+</svg>
+
+                Upload
+                </button>
+            </div>
+            <h1 className="text-2xl mt-4">Description</h1>
+            <p className="text-gray-500 text-sm">description of the places</p>
            </form>
         )}
     </div>
