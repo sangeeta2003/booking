fdd7a5fcb7bb9c955991ba8bbb1ba15282189e9c
*pknowledge* <mishrasangeeta944@gmail.com>
2024-06-05 11:02:05 +0530

    schema for my project

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,24 +1,28 @@
-import { useContext } from 'react';
-import { Link, useParams, Navigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { Link, useParams, Navigate, redirect } from 'react-router-dom';
 import { UserContext } from './UserContext';
 import axios from 'axios';
 
 const Account = () => {
-  const { user, ready } = useContext(UserContext);
+  const { user, ready ,setUser} = useContext(UserContext);
+  const[direct,setDirect] = useState(null)
   let { subpage } = useParams();
   if (subpage === undefined) {
     subpage = 'profile';
+    
   }
 
   async function logout() {
-    await axios.get('/logout');
+    await axios.post('/logout');
+    setDirect('/')
+    setUser(null);
   }
 
   if (!ready) {
     return 'Loading...';
   }
 
-  if (ready && !user) {
+  if (ready && !user && !direct) {
     return <Navigate to="/login" />;
   }
 
@@ -46,7 +50,8 @@ const Account = () => {
       {subpage === 'profile' && (
         <div className="text-center w-full  mx-auto">
           Logges in as {user.name} ({user.email}) <br></br>
-          <button className="bg-primary text-white w-1/4 py-2 px-6 rounded-full  mt-2">
+          <button onClick={logout}
+          className="bg-primary text-white w-1/4 py-2 px-6 rounded-full  mt-2">
             Logout
           </button>
         </div>
---
+++ server/models/Place.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+
+const PlaceSchema = new mongoose.Schema({
+    owner:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    title:String,
+    description:String,
+    Photos:[String],
+    address:String,
+    perks:[String],
+    extraInfo :String,
+checkIn:Number,
+checkOut:Number,
+maxGuests:Number,
+price:Number
+
+
+
+});
+
+const PlaceModel = mongoose.model('Place',PlaceSchema)
+module.exports= PlaceModel;
